f11091e0823d44d56660ec3435732efcf6b385a7
matheuslima25 <josematheuslima25@gmail.com>
2023-04-25 16:18:29 -0300

    fix: only the material to class 1

--- src/main/java/br/edu/unifip/ecommerceapi/models/Product.java
+++ src/main/java/br/edu/unifip/ecommerceapi/models/Product.java
@@ -4,19 +4,45 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.UUID;
 
 @Entity
-@Table(name = "TB_PRODUCT")
 public class Product implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    private Long id;
     private String name;
     private String description;
     private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 
 }
