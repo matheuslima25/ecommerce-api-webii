f8714043c1361a683cc3344044a2d012d9b08dd4
matheuslima25 <josematheuslima25@gmail.com>
2023-05-02 14:00:12 -0300

    feat: namedquery modular

--- src/main/java/br/edu/unifip/ecommerceapi/repositories/ProductRepository.java
+++ src/main/java/br/edu/unifip/ecommerceapi/repositories/ProductRepository.java
@@ -1,7 +1,9 @@
 package br.edu.unifip.ecommerceapi.repositories;
 
 import br.edu.unifip.ecommerceapi.models.Product;
+import jakarta.persistence.NamedQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +18,8 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
     void delete(Product product);
 
     List<Product> findByActiveTrue();
-
+    @Query("SELECT p FROM Product p WHERE p.name = :name")
     List<Product> findByName(@Param("name") String name);
+    @Query("SELECT p FROM Product p JOIN p.category c WHERE c.name = :name")
     List<Product> findByCategoryName(@Param("name") String name);
 }
